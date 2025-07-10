d2b808dccbf7ce46c9f0cfc815819ae0e7634bd1
shifyannn <asifyan01@gmail.com>
2025-07-10 09:56:18 +0700

    finish booking route

--- src/app/api/booking/route.js
+++ src/app/api/booking/route.js
@@ -1,12 +1,39 @@
 import uniqueNumb from "../../../../lib/nanoid";
+import prisma from "../../../../lib/prisma";
 
 export async function POST(request) {
   const order = await request.json();
-  const numb = uniqueNumb();
-  console.log(numb);
+  let invoiceNumber = `INV-${uniqueNumb()}`;
+  let {
+    name,
+    email,
+    phone,
+    packageId,
+    date,
+    sessionNumbers,
+    notes,
+    paymentMethod,
+  } = order;
+  const booking = await prisma.booking.create({
+    data: {
+      invoiceNumber,
+      name,
+      email,
+      phone,
+      packageId,
+      date: date,
+      sessionNumbers,
+      notes,
+      paymentMethod,
+      status: "PENDING",
+    },
+  });
   return Response.json([
     {
       status: "ok",
+      data: {
+        booking,
+      },
     },
   ]);
 }
