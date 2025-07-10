e9e0b5aaf2c440c4af6cf4d388eb52ff4ef7c18e
shifyannn <asifyan01@gmail.com>
2025-07-10 11:38:26 +0700

    create verivication logic

--- src/app/api/booking/route.js
+++ src/app/api/booking/route.js
@@ -1,8 +1,9 @@
 import uniqueNumb from "../../../../lib/nanoid";
 import prisma from "../../../../lib/prisma";
+import { BookingStatus } from "@/generated/prisma";
 
 export async function POST(request) {
-  const order = await request.json();
+  const body = await request.json();
   let invoiceNumber = `INV-${uniqueNumb()}`;
   let {
     name,
@@ -13,7 +14,48 @@ export async function POST(request) {
     sessionNumbers,
     notes,
     paymentMethod,
-  } = order;
+  } = body;
+
+  // Cek Kelengkapan Data
+  if (
+    !name ||
+    !email ||
+    !phone ||
+    !packageId ||
+    !date ||
+    !sessionNumbers?.length ||
+    !paymentMethod
+  ) {
+    return new Response(JSON.stringify({ message: "Data tidak lengkap" }), {
+      status: 400,
+    });
+  }
+  // create booking date
+  const bookingDate = new Date(date);
+  bookingDate.setHours(0, 0, 0, 0);
+
+  // Check if session is booked
+  const existing = await prisma.booking.findMany({
+    where: {
+      date: bookingDate,
+      sessionNumbers: { hasSome: sessionNumbers },
+      status: {
+        in: [
+          BookingStatus.PENDING,
+          BookingStatus.PAID,
+          BookingStatus.COMPLETED,
+        ],
+      },
+    },
+  });
+
+  if (existing.length > 0) {
+    return new Response(JSON.stringify({ message: "Sesi sudah dibooking" }), {
+      status: 409,
+    });
+  }
+
+  // insert data to database
   const booking = await prisma.booking.create({
     data: {
       invoiceNumber,
@@ -21,7 +63,7 @@ export async function POST(request) {
       email,
       phone,
       packageId,
-      date: date,
+      date: bookingDate,
       sessionNumbers,
       notes,
       paymentMethod,
