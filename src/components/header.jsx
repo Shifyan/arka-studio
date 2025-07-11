908507bccce30b41437024f925202545f4f0db75
shifyannn <asifyan01@gmail.com>
2025-07-11 11:35:51 +0700

    create navbar component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,68 @@
+"use client";
+import { Prociono } from "next/font/google";
+import { Button } from "./ui/button";
+import Link from "next/link";
+const prociono = Prociono({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function Navbar() {
   return (
-    <div>
-      <h1 className="font-header">Hellon World</h1>
+    <div className="flex flex-row">
+      <div className="left-header basis-1/5 ps-[114px]  pt-[13px]">
+        <div className="w-fit ">
+          <h1
+            className={`${prociono.className} text-[28px] leading-[28px] text-right`}
+          >
+            Arka <br />
+            Studio
+          </h1>
+        </div>
+      </div>
+
+      <div className="center-header flex flex-row basis-3/5 justify-center pt-[25px]">
+        <Link href="/">
+          <Button
+            variant="link"
+            className="cursor-pointer text-[20px] font-semibold "
+          >
+            Home
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button
+            variant="link"
+            className="cursor-pointer text-[20px] font-semibold  "
+          >
+            Services
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button
+            variant="link"
+            className="cursor-pointer text-[20px] font-semibold "
+          >
+            About Us
+          </Button>
+        </Link>
+        <Link href="/">
+          <Button
+            variant="link"
+            className="cursor-pointer text-[20px] font-semibold "
+          >
+            Portofolio
+          </Button>
+        </Link>
+      </div>
+      <div className="right-header basis-1/5 pt-[11px]">
+        <Button
+          variant="outline"
+          className="font-medium text-[20px] px-[28px] py-[16px] leading-[29px]"
+        >
+          Hire Now
+        </Button>
+      </div>
     </div>
   );
 }
